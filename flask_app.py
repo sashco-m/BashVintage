5064e8403268d9d8987a383bfb0f79aa27413d28
sashco <sashco.mistelbacher@gmail.com>
2020-09-06 19:52:54 +0000

    added the tables to the database

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 import os
 from flask import Flask, render_template, url_for, redirect, request, session
-from flask_session.__init__ import Session
+from flask_session import Session
 from tempfile import mkdtemp
 from flask_sqlalchemy import SQLAlchemy
 
@@ -31,6 +31,28 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db=SQLAlchemy(app)
+
+class Stock(db.Model):
+    __tablename__="stock"
+
+    id=db.Column(db.Integer, primary_key=True)
+    list_date=db.Column(db.DateTime)
+    purchase_date=db.Column(db.DateTime)
+    title=db.Column(db.String(100))
+    cover_image=db.Column(db.String(2048))
+    alt_image_1=db.Column(db.String(2048))
+    alt_image_2=db.Column(db.String(2048))
+    price=db.Column(db.Float)
+    description=db.Column(db.Text)
+    size=db.Column(db.Integer)
+
+class Users(db.Model):
+    __tablename__="users"
+
+    user_id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.Text)
+    hash=db.Column(db.Text)
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
