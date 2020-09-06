b1f0cf9617c14abddd35ae8b205e7d52ade85ebc
sashco <sashco.mistelbacher@gmail.com>
2020-09-06 19:12:59 +0000

    Adding templates, troubleshooting routes

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,17 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+#debug mode
+app.config["DEBUG"] = True
 
+@app.route('/')
+def index():
+    return render_template("layout.html")
+
+@app.route("/checkout/<int:purchased>", methods=["GET", "POST"])
+def checkout(purchased):
+    return "temp"
